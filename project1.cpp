d3fbcb37e540a78e644213729ffa6741868dfd26
Andrea Anez <anez@csilvm-01.cs.ucsb.edu>
2020-10-14 09:54:16 -0700

    testing

---
+++ project1.cpp
@@ -0,0 +1,15 @@
+// project1.cpp
+
+//Andrea Anez 
+// CS 130 Divy 
+// 10/8/2020
+
+#include "project1.h"
+#include <iostream>
+using namespace std; 
+
+Tst::Tst(){
+    Node 
+    //intialize an empty tree 
+    // initalize the root 
+}
